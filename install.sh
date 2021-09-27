ff63b9709729a4d3c988e0b72d355744160ab4e1
DhivehiMv <admin@dhivehi.mv>
2021-09-27 09:07:08 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -35,4 +35,27 @@ sudo yum install https://repo.ius.io/ius-release-el7.rpm
 yum install mod_php73 php73-bcmath php73-cli php73-gd php73-ldap php73-mbstring php73-mysqlnd php73-soap
 
 
+#To install mysql v8
+sudo wget https://dev.mysql.com/get/mysql80-community-release-el7-3.noarch.rpm
+sudo md5sum mysql80-community-release-el7-3.noarch.rpm
+sudo rpm -ivh mysql80-community-release-el7-3.noarch.rpm
+sudo yum install mysql-server
+sudo systemctl start mysqld
+sudo systemctl enable mysqld
+
+#Find Temporary Password
+sudo grep 'temporary password' /var/log/mysqld.log
+
+#To set new mysql password
+sudo mysql_secure_installation
+
+#To fix pma login issue
+ALTER USER root@'%' IDENTIFIED WITH mysql_native_password BY 'pAssWord';
+
+
+
+
+
+
+
 
